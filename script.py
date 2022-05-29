3a235df4f8f0bfb6f28b9ae3a07024a66800ce98
bakkenal <>
2022-05-29 15:54:56 -0500

    ingredients parsed

--- script.py
+++ script.py
@@ -1,4 +1,22 @@
 import sys
+from bs4 import BeautifulSoup
+from bs4.element import Comment
+import urllib.request
+import requests
+
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+
+def text_from_html(body):
+    soup = BeautifulSoup(body, 'html.parser')
+    texts = soup.findAll(text=True)
+    visible_texts = filter(tag_visible, texts)  
+    return u" ".join(t.strip() for t in visible_texts)
 
 def printHelp():
     print("\n\nCS337 project - recipe parser")
@@ -17,25 +35,34 @@ For example - \"python script.py style chinese [url]\"\n\n")
 def parseRecipe():
     pass
 
-if len(sys.argv) == 1 or len(sys.argv) > 4:
+if len(sys.argv) != 4:
     # print help statement
     printHelp()
 
 arguments = sys.argv[1:]
-print(arguments)
+# print(arguments)
 for i in range(len(arguments)):
     arguments[i] = arguments[i].lower()
 transform = arguments[0]
-if len(arguments) == 3:
-    mode = arguments[1]
-    url = arguments[2]
-else:
-    printHelp()
+mode = arguments[1]
+url = arguments[2]
 
 styles = ["chinese", "italian"]
 
 # parse website url
+webpage = requests.get(url, timeout=5)
+doc = BeautifulSoup(webpage.text, "html.parser")
+# ingredients = doc.find_all("section", {"class": "recipeIngredients"})
+# test = ingredients.find("ingredients-item")
+ingredients = []
+ingredientParse = doc.find_all("span", {"class": 'ingredients-item-name'})
+print(ingredientParse)
+for element in ingredientParse: 
+    print(element.text)
+    ingredients.append(element.text)
 
+# html = urllib.request.urlopen('https://www.allrecipes.com/recipe/24074/alysias-basic-meat-lasagna/').read()
+# print(text_from_html(html))
 
 # apply transformation
 if transform == "vegetarian":
