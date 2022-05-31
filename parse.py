8de38d175c40e89cb246e7b41f0e988d81dd67a2
bakkenal <>
2022-05-31 17:34:32 -0500

    finish parser - todo: make json

--- parse.py
+++ parse.py
@@ -4,6 +4,7 @@ from bs4.element import Comment
 import requests
 import urllib.request
 import unicodedata
+import json
 
 measurements = ["tablespoons", "teaspoons", "pounds", "cups", "cans", "packages", "jars", "ounces", "containers", "bushels", "tons", \
     "tablespoon", "teaspoon", "pound", "cup", "can", "package", "jar", "ounce", "container", "bushel", "ton"]
@@ -16,12 +17,12 @@ def parse(url):
     # get ingredients
     ingredients = []
     ingredientParse = doc.find_all("span", {"class": 'ingredients-item-name'})
-    print("printing ingredients")
     for element in ingredientParse: 
         ingredients.append(element.text)
     quantities = []
+    parsed_ingredients = []
     for ingredient in ingredients:
-
+        ingredient
         quantity = []
         ingredient_name = ""
 
@@ -29,25 +30,35 @@ def parse(url):
         alternateQuantity = ""
         if not (ingredient[0].isalpha() or ingredient[0] == " "):
             quantity = [unicodedata.numeric(ingredient[0])]
+            ingredient = ingredient[1:]
             if "(" in ingredient:
                 alternateQuantity = ingredient.split('(')[1].split(')')[0]
+                # remove it from ingredient
+                ingredient = ingredient.split('(')[0] + ingredient.split(')')[1]
             if alternateQuantity != "":
                 quantity.append(alternateQuantity)
     
         if "," in ingredient:
             separated = ingredient.split(",")
+            ingredient = separated[0]
             additional_directions = separated[1]
 
+        check = 0
         for measurement in measurements:
             if measurement in ingredient:
+                check = 1
                 quantity[0] = str(quantity[0]) + " " + measurement
                 ingredient_name = ingredient.split(measurement)[1].strip()
+                parsed_ingredients.append(ingredient_name.strip())
                 print(quantity[0])
                 print(ingredient_name)
                 break
+        if not check:
+            parsed_ingredients.append(ingredient.strip())
         
         quantities.append(quantity)
 
+    print(parsed_ingredients)
     print(quantities)
 
     # get directions
